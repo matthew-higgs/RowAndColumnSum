ed018877a36741573429af8a1374a5b554f6441e
Chewbacca <higgimat000@warren.k12.in.us>
2025-01-14 13:38:07 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int colSum = 0;
+        for (int col = 0; col < array[0].length; col++) {
+            for (int row = 0; row < array.length; row++) {
+                colSum += array[row][col];
+            }
+        }
+        System.out.println(colSum);
+    }
 }
